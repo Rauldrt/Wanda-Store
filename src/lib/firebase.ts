b3cc7efec19a16cb9343e927f979ac7f986158d2
Rauldrt <rauldrt5@gmail.com>
2026-06-06 23:14:12 -0300

    chore: actualizar credenciales y proyecto de Firebase a wanda-store-raul-2026

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "wanda-pro-cloud-raul-123",
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:557004412716:web:a34b41d975dfd6d22bbe89",
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "wanda-pro-cloud-raul-123.firebasestorage.app",
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCEYIIw0SA2UkHtf2vI90_c9hhFEco6uqk",
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "wanda-pro-cloud-raul-123.firebaseapp.com",
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "557004412716",
-    projectNumber: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_NUMBER || "557004412716"
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "wanda-store-raul-2026",
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1068696357909:web:6eeabfec2e35954105121c",
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "wanda-store-raul-2026.firebasestorage.app",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDbBU3AtliOxTZKVoYbf7x5Oqf0TuAcKe4",
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "wanda-store-raul-2026.firebaseapp.com",
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1068696357909",
+    projectNumber: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_NUMBER || "1068696357909"
 };
 
 const app = initializeApp(firebaseConfig);
